df5fab27285daa3e4f17016fcba18c62f619ba04
johndifini <johndifini@gmail.com>
2020-02-06 12:24:44 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,12 @@
+const config = {
+  destination: {
+    name: 'stormy.emf',
+    mimeType: 'application/x-msmetafile'
+  },
+  source: {
+    path: '/Users/johndifini/Downloads/storm2.emf'
+  }
+};
+
+module.exports = config;
+
